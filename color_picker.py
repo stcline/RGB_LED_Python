318b6213dd557c6f2da7664244ec43f09914ae24
stcline <steve.cline@weldre4.org>
2023-02-04 19:49:06 -0700

    Add two new scritps

---
+++ color_picker.py
@@ -0,0 +1,45 @@
+#This script asks for a duty cycle value for each color of an 
+#RGB LED and displays that color for 3 seconds, then repeats.
+
+#libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+#disable warnings (optional)
+GPIO.setwarnings(False)
+
+#Select GPIO Mode
+GPIO.setmode(GPIO.BCM)
+
+#Set pins
+redPin = 17
+greenPin = 27
+bluePin = 22
+
+#Set pins as output
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+red = GPIO.PWM(redPin, 75)
+green = GPIO.PWM(greenPin, 75)
+blue = GPIO.PWM(bluePin, 75)
+
+def setColor ():
+
+	red_val = int(input("What duty cycle for the red LED?"))
+	green_val = int(input("What duty cycle for the green LED?"))
+	blue_val = int(input("What duty cycle for the blue LED?"))
+
+	red.start(red_val/2.55)
+	green.start(green_val/2.55)
+	blue.start(blue_val/2.55)
+
+	sleep (3)
+
+	red.start(0)
+	green.start(0)
+	blue.start(0)
+
+while True:
+        setColor()
