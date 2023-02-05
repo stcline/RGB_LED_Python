36d24fcecb100f722b3f065e983134b5f4f03867
stcline <steve.cline@weldre4.org>
2023-02-05 09:14:13 -0700

    Modify RGB_Contol.py to accept input

--- RGB_Control.py
+++ RGB_Control.py
@@ -1,10 +1,21 @@
 #This script uses the RGB module to control an RGB LED from user input
 
 from RGB import RGB
+from time import sleep
 
 #Set the board to GPIO BCM
 RGB.boardSetup()
 #Create an RGB object
 rgb = RGB(17,27,22)
+
+rval = int(input(" What duty cycle would you like to use for red? "))
+gval = int(input(" What duty cycle would you like to use for green? "))
+bval = int(input(" Whad duty cycle would you like to use for blue? "))
+dur = int(input(" How long would you like it to stay on? "))
+
+print()
+print(" Here is your custom color...")
+print()
+
 #Turn on the rgb
-rgb.colorOn(255,0,0,3)
+rgb.colorOn(rval,gval,bval,dur)
